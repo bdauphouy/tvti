04330fd0e121fd600d30ac313748b30c4e059863
Baptiste Dauphouy <bdauphouy@gmail.com>
2025-05-19 18:54:06 +0200

    fix: update stats

--- src/components/Stats.svelte
+++ src/components/Stats.svelte
@@ -5,9 +5,9 @@
 <section class="mx-auto max-w-screen-3xl px-6 pt-20 md:px-20">
 	<div class="rounded-xl bg-white/5 p-8 md:p-10">
 		<ul class="mx-auto flex w-full max-w-4xl flex-wrap justify-center gap-10 md:justify-between">
-			<Stat number={8} label="Ans d'existence" />
+			<Stat number={10} label="Ans d'existence" />
 			<Stat number={25} label="Ans d'expérience" />
-			<Stat number="+200" label="Chantiers réalisés" />
+			<Stat number="+250" label="Chantiers réalisés" />
 		</ul>
 	</div>
 </section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-	import KnowHow from 'components/KnowHow.svelte'
 	import Domains from 'components/Domains.svelte'
 	import Footer from 'components/Footer.svelte'
 	import Homepage from 'components/Homepage.svelte'
+	import KnowHow from 'components/KnowHow.svelte'
 	import Partners from 'components/Partners.svelte'
 	import Stats from 'components/Stats.svelte'
 
